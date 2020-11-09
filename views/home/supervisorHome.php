bf187abae1c132e117b62f001b3e38128f10866f
Thom Mondeaux <Thommond554@gmail.com>
2020-11-09 09:43:51 -0500

    Created all the home pages for the different type of user roles.
    Added the auth directory so all authentication like login and register 
    views will be held there.
    
    Added some basic styles to improve user readability.

---
+++ views/home/supervisorHome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Supervisors Home</title>
+  </head>
+  <body>
+    <p>Welcome Home Supervisor!</p>
+  </body>
+</html>
---
+++ views/landing.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="../static/style.css">
+  </head>
+  <body>
+
+    <section>
+
+    <h1>Welcome to Retire Inspired!</h1>
+
+    <a href="http://localhost/Retire-Inspired/views/auth/register.php">Register</a>
+
+    <a href="http://localhost/Retire-Inspired/views/auth/login.php">Log In</a>
+
+    </section>
+
+
+  </body>
+</html>
