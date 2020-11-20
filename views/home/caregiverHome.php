b18c6dac4a8977991fec46d079796f7c27075294
Brendan Horst <yoshistar2000@gmail.com>
2020-11-20 10:09:29 -0500

    Started table on caregiverHome and created database connection

--- views/home/caregiverHome.php
+++ views/home/caregiverHome.php
@@ -34,5 +34,55 @@
 
     </section>
 
+    <section class="schedule">
+
+      <form class="manageSchedules" action="caregiverHome.php" method="post">
+
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Morning Medicine</th>
+            <th>Afternoon Medicine</th>
+            <th>Night Medicine</th>
+            <th>Breakfast</th>
+            <th>Lunch</th>
+            <th>Dinner</th>
+          </tr>
+          <tr>
+
+            <?php
+            #establish some variables
+            $schedule = [];
+            $patient_name = '';
+
+            #use the submitted date or default to today
+            if (isset($_POST['search'])) {
+              $date = $_POST['date'];
+            }
+            else {
+              $date = date('Y-m-d');
+            }
+
+            $id = $_SESSION['id'];
+
+            $link = mysqli_connect("localhost", "root", "", "retire");
+
+            if ($link == false) {
+              die("ERROR: Could not connect. " . mysqli_connect_error());
+            }
+
+            #Check which group the caregiver is assigned to
+
+            #Get all of the patients in that group
+
+            #Display the schedule for each patient
+
+            mysqli_close($link);
+            ?>
+          </tr>
+        </table>
+      </form>
+    </section>
+
   </body>
 </html>
